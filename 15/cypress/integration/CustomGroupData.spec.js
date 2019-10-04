afd4f451561780a5e8256a4a1fa831e53ef45e91
Tung Lam <tungtobi@gmail.com>
2019-10-04 15:41:44 +0700

    fix bug multi element

--- 15/cypress/integration/CustomGroupData.spec.js
+++ 15/cypress/integration/CustomGroupData.spec.js
@@ -6,7 +6,7 @@ describe('Chỉnh sửa danh sách thành viên nhóm', () => {
     it("Thêm sinh viên vào nhóm", function() {
         cy.visit('https://hoclieu.sachmem.vn/live_class/class/5d96f5e6ec4e443482b3c207/list_groups')
 
-        cy.get("table.table.table-border > tbody > tr > td:nth-child(3) > i.fa.fa-edit.pl-3.text-blue-custom").click();
+        cy.get("table.table.table-border > tbody > tr > td:nth-child(3) > i.fa.fa-edit.pl-3.text-blue-custom").first().click();
 
         cy.contains("Chọn học sinh").parent().click();
 
@@ -20,7 +20,7 @@ describe('Chỉnh sửa danh sách thành viên nhóm', () => {
     it("Xóa sinh viên trong nhóm", function() {
         cy.visit('https://hoclieu.sachmem.vn/live_class/class/5d96f5e6ec4e443482b3c207/list_groups')
 
-        cy.get("table.table.table-border > tbody > tr > td:nth-child(3) > i.fa.fa-edit.pl-3.text-blue-custom").click();
+        cy.get("table.table.table-border > tbody > tr > td:nth-child(3) > i.fa.fa-edit.pl-3.text-blue-custom").first().click();
 
         cy.get(".ng-value-icon").contains("×").first().click();
 

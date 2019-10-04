96472bd9917d96eab23c5e1116ea9c491d8fa3ef
Tung Lam <tungtobi@gmail.com>
2019-10-04 14:54:01 +0700

    test done

--- 15/cypress/integration/demo.spec.js
+++ 15/cypress/integration/demo.spec.js
@@ -1,4 +1,4 @@
-describe('Thêm sinh viên vào nhóm', () => {
+describe('Chỉnh sửa danh sách thành viên nhóm', () => {
     beforeEach(() => {
         cy.login();
     })
@@ -6,14 +6,23 @@ describe('Thêm sinh viên vào nhóm', () => {
     it("Thêm sinh viên vào nhóm", function() {
         cy.visit('https://hoclieu.sachmem.vn/live_class/class/5d96f5e6ec4e443482b3c207/list_groups')
 
-        // cy.get("li:nth-child(1) > div.w-100 > div.mt-1 > a").click();
-
-        // cy.get("i.fa.fa-group").click(); 
         cy.get("table.table.table-border > tbody > tr > td:nth-child(3) > i.fa.fa-edit.pl-3.text-blue-custom").click();
 
         cy.contains("Chọn học sinh").parent().click();
 
         cy.get(".ng-option").not(".ng-option-selected").first().click();
+
+        cy.contains("Lưu").click();
+
+        cy.get('div#toast-container').should('exist')
+    })
+
+    it("Xóa sinh viên trong nhóm", function() {
+        cy.visit('https://hoclieu.sachmem.vn/live_class/class/5d96f5e6ec4e443482b3c207/list_groups')
+
+        cy.get("table.table.table-border > tbody > tr > td:nth-child(3) > i.fa.fa-edit.pl-3.text-blue-custom").click();
+
+        cy.get(".ng-value-icon").contains("×").first().click();
 
         cy.contains("Lưu").click();
 

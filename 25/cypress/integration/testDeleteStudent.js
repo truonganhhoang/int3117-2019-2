fd55cbffdc4a53db06526398cc624936f2d4cc7f
vukihai <vukihai98@gmail.com>
2019-10-04 14:55:54 +0700

    add assert for delete all student

--- 25/cypress/integration/testDeleteStudent.js
+++ 25/cypress/integration/testDeleteStudent.js
@@ -7,59 +7,61 @@ describe("Test delete student", function () {
         cy.get('input[type="submit"]').click();
     });
     afterEach(function () {
-	cy.wait(1000)
-	cy.get('[class="ng-input"]').eq(1).click()
-	cy.wait(500)
-	cy.contains('Đã xóa').parent().click()
-	cy.wait(500)
-	cy.get("body").then($body => {
-        if ($body.find('[data-target="#activeStudent"]').length > 0) { 
-		cy.get('[data-target="#activeStudent"]').each(($el, index, $list) => {
-			cy.wait(500)
-			cy.wrap($el).click()
-			cy.wait(500)
-			cy.get('[class="btn btn-primary bootbox-accept"]').click()
+		cy.wait(1000)
+		cy.get('[class="ng-input"]').eq(1).click()
+		cy.wait(500)
+		cy.contains('Đã xóa').parent().click()
+		cy.wait(500)
+		cy.get("body").then($body => {
+		if ($body.find('[data-target="#activeStudent"]').length > 0) { 
+			cy.get('[data-target="#activeStudent"]').each(($el, index, $list) => {
+				cy.wait(500)
+				cy.wrap($el).click()
+				cy.wait(500)
+				cy.get('[class="btn btn-primary bootbox-accept"]').click()
 			})
-	        }
+	    }
 	});
 
   })
+  it("delete all student", () => {
+        cy.visit("https://hoclieu.sachmem.vn/live_class/class/5cfdf60a81522b4e06d1f9fc/students");
+		cy.wait(500)
+		var studentNum
+		cy.get("body").then($body => {
+			studentNum = $body.find('[data-target="#deleteStudent"]').length
+			for (var i = 0 ; i <studentNum; i++){
+				cy.get('[data-target="#deleteStudent"]').eq(0).click()
+				cy.wait(500)
+				cy.get('[class="btn btn-primary bootbox-accept"]').click()
+				cy.wait(1000)
+			}
+		})
+		cy.get("body").find('[data-target="#deleteStudent"]').should('have.length', 0)
+    });
     it("delete first student in the student list", () => {
         cy.visit("https://hoclieu.sachmem.vn/live_class/class/5cfdf60a81522b4e06d1f9fc/students");
-	cy.wait(500)
-	cy.get('[data-target="#deleteStudent"]').eq(1).click()
-	cy.contains("Xác nhận").click()
-	cy.wait(500)
-	cy.get('[class="bootbox-close-button close"]').click()
-	
+		cy.wait(500)
+		cy.get('[data-target="#deleteStudent"]').eq(1).click()
+		cy.contains("Xác nhận").click()
+		cy.wait(500)
+		cy.get('[class="bootbox-close-button close"]').click()
+		
     });
     it("delete last student in the student list", () => {
         cy.visit("https://hoclieu.sachmem.vn/live_class/class/5cfdf60a81522b4e06d1f9fc/students");
-	cy.wait(500)
-	cy.get('[data-target="#deleteStudent"]').last().click()
-	cy.contains("Xác nhận").click()
-	cy.wait(500)
-	cy.get('[class="bootbox-close-button close"]').click()
+		cy.wait(500)
+		cy.get('[data-target="#deleteStudent"]').last().click()
+		cy.contains("Xác nhận").click()
+		cy.wait(500)
+		cy.get('[class="bootbox-close-button close"]').click()
 
     });
     it("delete student without confirm", () => {
         cy.visit("https://hoclieu.sachmem.vn/live_class/class/5cfdf60a81522b4e06d1f9fc/students");
-	cy.get('[data-target="#deleteStudent"]').last().click()
-	cy.wait(500)
-	cy.get('[class="bootbox-close-button close"]').click()
+		cy.get('[data-target="#deleteStudent"]').last().click()
+		cy.wait(500)
+		cy.get('[class="bootbox-close-button close"]').click()
     });
-    it("delete all student", () => {
-        cy.visit("https://hoclieu.sachmem.vn/live_class/class/5cfdf60a81522b4e06d1f9fc/students");
-	cy.wait(500)
-	var studentNum
-	cy.get("body").then($body => {
-		studentNum = $body.find('[data-target="#deleteStudent"]').length
-		for (var i = 0 ; i <studentNum; i++){
-			cy.get('[data-target="#deleteStudent"]').eq(0).click()
-			cy.wait(500)
-			cy.get('[class="btn btn-primary bootbox-accept"]').click()
-			cy.wait(1000)
-		}
-	});
-    });
+	
 }); 

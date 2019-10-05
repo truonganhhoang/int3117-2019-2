94cd4d659b9e5c4751892e9939ac97b583acea45
honghoa1 <43260954+honghoa1@users.noreply.github.com>
2019-10-05 20:46:44 +0700

    Delete sampleTestSpace.js

--- nhóm 22/hoclieu/cypress/integration/sampleTestSpace.js
+++
@@ -1,7 +0,0 @@
-describe('Test 1', function() {
-    it('clicking "type" navigates to a new url', function() {
-      cy.visit('https://example.cypress.io/commands/querying')
-  
-      cy.get('.btn-primary').should('contain', 'Button')
-    })
-  })

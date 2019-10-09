d50d5d0a557c6e0d17fb372562715487db77b563
S2kael <laiducminh1002@gmail.com>
2019-10-10 03:26:08 +0700

    Test for funtions chat with teacher
    Test for funtions chat with teacher

---
+++ web_testing_group_23/cypress/integration/test/message.spec.js
@@ -0,0 +1,32 @@
+const classes = [
+    "Kiểm thử 2019-2020",
+    "Lớp 22"
+]
+describe('Test chức năng nhắn tin với giáo viên', function () {
+    // npm run cypress:open
+    beforeEach(() => {
+        cy.login();
+        cy.contains('Lớp học của tôi').click();
+        cy.url().should('include', 'https://hoclieu.sachmem.vn/live_class/classes');
+        let cls = classes[1];
+        cy.contains(cls).click({force:true});
+        let cls_name = cy.get('ol.breadcrumb.box-shadow-hide-right').should('contain',cls);
+        cy.contains("Nhắn tin, trao đổi").click().as('message');
+        cy.wait(1000);
+    })
+    context('Window', function () {
+        it('Nhắn tin thành công', function () {
+            cy.contains("laiducminh1002@gmail.com").click();
+            let message = "test cypress";
+            cy.get('.send-message').type(message);
+            cy.get('button').contains('Gửi').click();
+            cy.wait(1000);
+            let my_message = cy.get('.message-box').get('.me').last().children().first();
+            my_message.should('contain',message);
+
+        })
+        it('Để trống tin nhắn', function (){
+            cy.get('button').contains('Gửi').should('be.disabled');
+        })
+    })
+});
--- web_testing_group_23/cypress/support/commands.js
+++ web_testing_group_23/cypress/support/commands.js
@@ -23,3 +23,21 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+const users = [
+    {
+        "email" : "laiducminh1002@gmail.com",
+        "password" : "Ineedyou~123"
+    },
+    {
+        "email" : "giaovien1@sachmem.vn",
+        "password" : "giaovien1@123"
+    }
+]
+Cypress.Commands.add('login', () => {
+    cy.visit('https://hoclieu.sachmem.vn');
+    cy.contains('Đăng nhập').click();
+    let user = users[1];
+    cy.get('#user_email').type(user.email);
+    cy.get('#user_password').type(user.password).type("{enter}");
+}) 
